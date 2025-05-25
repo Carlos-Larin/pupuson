b7228ac985328e0c4e32f52c5a52908fadb2f369
Carlos-Larin <nekokar701@gmail.com>
2025-05-25 15:40:51 -0600

    a

--- nivel1/escalera.gd
+++ nivel1/escalera.gd
@@ -4,4 +4,4 @@ func _on_ESCALERA_body_entered(body):
 	if body.name == "player1":
 		# Cambia estas coordenadas por la posición superior deseada
 		var posicion_superior = Vector2(1280, 500)
-		body.global_position = posicion_superior
+		body.global_position = posicion_superi or
